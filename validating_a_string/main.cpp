43b5fe3c86bb54cea5bee23b03f55b7e4633a275
ubyhzargam <ubyhzargam@gmail.com>
2023-06-22 17:08:39 +0530

    Update main.cpp

--- validating_a_string/main.cpp
+++ validating_a_string/main.cpp
@@ -6,9 +6,28 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+int main()
+{
+    char str[50];
+    int c=0;
+    cout<<"Enter a string to validate it : "<<endl;
+    cin>>str;
+    for(int i=0;i<50;i++)
+    {
+        if(str[i]=='\0')
+            break;
+        if((str[i]>=65&&str[i]<=90)||(str[i]>=97&&str[i]<=122)||(str[i]>=48&&str[i]<=57))
+            continue;
+        else
+        {
+            c++;
+            break;
+        }
+    }
+    if(c>0)
+        cout<<"The string given is not valid "<<endl;
+    else
+        cout<<"The given string is valid "<<endl;
     return 0;
 }
